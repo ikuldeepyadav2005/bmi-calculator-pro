57d670ac1e565baa6476038964518e0175fbfabf
ikuldeepyadav2005 <ikuldeepyadav2005@gmail.com>
2026-07-24 02:14:47 +0530

    Insert Google Ads script in layout component
    Added Google Ads script for ad integration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="1imq7mv0ijgjKCN7_qZYc9118p3xzvUp7rAROoVqPc4" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2325172875255272"
+     crossorigin="anonymous"></script>
       </head>
       <body className="bg-black text-white min-h-screen flex-col">
         
